ec169dc9b479cb1873fdd1cefa71c70272354e91
Mateusz Choma <kontakt@coderoad.pl>
2021-01-11 20:12:25 +0100

    App in unaware of its container

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import Textarea from './Textarea'
 
 export class App {
 
-    constructor(selector) {
-        this.container = document.querySelector(selector)
-
-        this.render()
-    }
+    constructor() {}
 
     render() {
 
-        this.container.innerHTML = ''
+        const container = document.createElement('div')
 
         const textareaElement1 = new Textarea(
             '{ "name": "Mateusz" }',
@@ -23,8 +19,10 @@ export class App {
             true
         )
 
-        this.container.appendChild(textareaElement1.render())
-        this.container.appendChild(textareaElement2.render())
+        container.appendChild(textareaElement1.render())
+        container.appendChild(textareaElement2.render())
+        
+        return container
 
     }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import App from './App'
 
-const app1 = new App('#root')
+const app1 = new App()
+document.querySelector('#root').appendChild(app1.render())
