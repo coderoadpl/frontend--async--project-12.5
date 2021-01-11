d42e97fa2a54eb16b88d1b6ea8cf3f606518f261
Mateusz Choma <kontakt@coderoad.pl>
2021-01-11 19:58:59 +0100

    Textarea with error indicator

--- src/Textarea.js
+++ src/Textarea.js
@@ -8,12 +8,23 @@ export class Textarea {
 
     render() {
 
+        const div = document.createElement('div')
         const textarea = document.createElement('textarea')
+        const p = document.createElement('p')
 
-        textarea.style.width = '100%' 
-        textarea.style.minHeight = '200px' 
+        textarea.style.width = '100%'
+        textarea.style.minHeight = '200px'
 
-        textarea.value = JSON.stringify(JSON.parse(this.value), null, 4)
+        let error = null
+        try {
+            textarea.value = JSON.stringify(JSON.parse(this.value), null, 4)
+        } catch (err) {
+            textarea.value = this.value
+            error = err
+        }
+
+        if(error) p.innerText = error.message
+
         if (this.readonly) textarea.setAttribute('readonly', 'true')
 
         textarea.addEventListener(
@@ -21,7 +32,10 @@ export class Textarea {
             (e) => this.onChange(e.target.value)
         )
 
-        return textarea
+        div.appendChild(textarea)
+        div.appendChild(p)
+
+        return div
 
     }
 
