151cb9ec32356942bf2d54d0d3029ef8030e8f1e
Mateusz Choma <kontakt@coderoad.pl>
2021-01-13 17:05:18 +0100

    Options added to Select component

--- src/Select.js
+++ src/Select.js
@@ -1,10 +1,29 @@
 export class Select {
 
-    constructor(options, value, onChange) { }
+    constructor(options, value, onChange) {
+
+        this.options = [
+            { label: 'First', value: 1 },
+            { label: 'Second', value: 2 },
+            { label: 'Third', value: 2 }
+        ]
+
+    }
 
     render() {
 
         const select = document.createElement('select')
+
+        this.options.forEach(({ label, value }) => {
+
+            const option = document.createElement('option')
+
+            option.value = value
+            option.innerText = label
+
+            select.appendChild(option)
+
+        })
 
         return select
 
